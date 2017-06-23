c1366b6eb6512459a2eed19a946ccfa889176918
Hassan <hassanalgoz@gmail.com>
2017-06-23 09:19:48 +0300

    changed news to posts

--- models/club.js
+++ models/club.js
@@ -3,14 +3,14 @@ var Schema = mongoose.Schema;
 
 // Club Schema
 var clubSchema = new mongoose.Schema({
-  name: {type: String, default: '', unique: true},
-  description: {type: String, default: ''},
-  logo: {type: String, default: ''},
-  members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  news: [{ type: Schema.Types.ObjectId, ref: 'News' }],
-  events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
-  date: { type: Date, default: Date.now },
-  condition: { type: String, default: 'unapproved' }
+    name: {type: String, default: '', unique: true},
+    description: {type: String, default: ''},
+    logo: {type: String, default: ''},
+    members: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    posts: [{ type: Schema.Types.ObjectId, ref: 'Post' }],
+    events: [{ type: Schema.Types.ObjectId, ref: 'Event' }],
+    date: { type: Date, default: Date.now },
+    condition: { type: String, default: 'unapproved' }
 });
 
 
