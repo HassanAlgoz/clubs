386825d9e5f9dff510207b8fd84d2d5573178bf6
Hassan <hassanalgoz@gmail.com>
2017-06-23 15:14:29 +0300

    patches here and there

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,35 +1,13 @@
 /* jshint browser:true */
 $(function(){
 
-	let url = document.URL;
-	let clubName = url.split('/')[4];
+	let clubId = getId('clubs')
 
-	if (clubName) {
-		if (clubName.charAt(clubName.length - 1) === "#") {
-			clubName = clubName.substring(0, clubName.length - 1);
-		}	
-	}
-	
-
-	$('ul.nav li.dropdown').hover(function() {
-		$(this).addClass('open');
-	}, function() {
-		$(this).removeClass('open');
-	});
-
-	if (clubName){
-		$('#nav-home').attr('href', `/clubs/${clubName}`);
-	} else {
-		$('#nav-home').attr('href', `/clubs`);
-	}
-
-	$('#nav-users').attr('href', `/clubs/${clubName}/manage/users`);
-	$('#nav-posts').attr('href', `/clubs/${clubName}/manage/posts`);
-	$('#nav-events').attr('href', `/clubs/${clubName}/manage/events`);
-	$('#nav-club').attr('href', `/clubs/${clubName}/edit`);
-
-	$('#nav-post-posts').attr('href', `/clubs/${clubName}/posts/x/edit`);
-	$('#nav-post-event').attr('href', `/clubs/${clubName}/events/x/edit`);
-	$('#nav-email').attr('href', `/clubs/${clubName}/email`);
+	$('#nav-users').attr('href', `/clubs/${clubId}/manage/users`);
+	$('#nav-posts').attr('href', `/clubs/${clubId}/manage/posts`);
+	$('#nav-events').attr('href', `/clubs/${clubId}/manage/events`);
+	$('#nav-club').attr('href', `/clubs/${clubId}/edit`);
+	$('#nav-post-posts').attr('href', `/clubs/${clubId}/post-new`);
+	$('#nav-post-event').attr('href', `/clubs/${clubId}/event-new`);
 
 });
