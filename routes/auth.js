114dae5474e261c026b0fc84b9737462b6f1a5a9
Hassan <hassanalgoz@gmail.com>
2017-07-10 21:01:44 +0300

    Attempted to make the id as the KFUPM ID

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,20 @@ const User = require('../models/user');
 router.get('/signup', (req, res, next) => res.render('signup'))
 router.post('/signup', (req, res, next) => {
 
+	// let id = String(req.body.KFUPMID)
+	// // Trim
+	// id = id.trim()
+	// // Drop the s if its there
+	// if (id.charAt(0).toLowerCase() === 's') {
+	// 	id = id.slice(1)
+	// }
+	// // Pass if the id is a number
+	// if (isNAN(Number(id))) {
+	// 	return next(new Error("KFUPMID passed is not a number"))
+	// }
+
 	User.register(new User({
+		// _id: req.body.KFUPMID,
 		username: req.body.username,
 		email: req.body.email,
 		major: req.body.major,
