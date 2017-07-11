5c287e5c91583aac458502f9b0b7acd01180f086
Hassan <hassanalgoz@gmail.com>
2017-07-11 20:02:17 +0300

    club is now plural

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@ $(function(){
             let clubs = data.clubs;
             console.log("clubs = ", clubs);
             for(let i = 0; i < clubs.length; ++i) {
-                $('#clubs').append(`<div class = "col-md-6"><a href="/club/${clubs[i]._id}">${clubs[i].name}</a></div>`);
+                $('#clubs').append(`<div class = "col-md-6"><a href="/clubs/${clubs[i]._id}">${clubs[i].name}</a></div>`);
             }
         },
         error: function(error) {
