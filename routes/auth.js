371c5107ef15a643612d3b5372aea1d5bb4c062c
Hassan <hassanalgoz@gmail.com>
2017-07-13 19:38:14 +0300

    Fixing authorization issues in passport.js

--- routes/auth.js
+++ routes/auth.js
@@ -7,57 +7,39 @@ const User = require('../models/user');
 
 
 //  Signup ====================================================================
-router.get('/signup', (req, res, next) => res.render('signup'))
-router.post('/signup', (req, res, next) => {
+router.get('/signup', function(req, res) {
+	res.render('signup', { message: req.flash('signupMessage') });
+});
 
-	// let id = String(req.body.KFUPMID)
-	// // Trim
-	// id = id.trim()
-	// // Drop the s if its there
-	// if (id.charAt(0).toLowerCase() === 's') {
-	// 	id = id.slice(1)
-	// }
-	// // Pass if the id is a number
-	// if (isNAN(Number(id))) {
-	// 	return next(new Error("KFUPMID passed is not a number"))
-	// }
-
-	User.register(new User({
-		// _id: req.body.KFUPMID,
-		username: req.body.username,
-		email: req.body.email,
-		major: req.body.major,
-		enrollment: req.body.enrollment
-	}), req.body.password, function(err, user) {
-        if (err) {
-            return next(err)
-        }
-
-        passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
-        });
-    });
-
-})
+router.post('/signup', passport.authenticate('local-signup', {
+	successRedirect : '/',
+	failureRedirect : '/auth/signup',
+	failureFlash : true // allow flash messages
+}));
 
 // Login ====================================================================
 router.get('/login', (req, res, next) => {
 	if (req.user) {
 		res.redirect('/')
 	} else {
-		res.render('login')
+		res.render('login', { message: req.flash('loginMessage') })
 	}
 })
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
-});
+router.post('/login', passport.authenticate('local-login', {
+	successRedirect : '/',
+	failureRedirect : '/auth/login',
+	failureFlash : true // allow flash messages
+}));
 
 
+// LOGOUT ==============================
 router.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
+	req.logout();
+	res.redirect('/');
 });
+
+
 
 
 module.exports = router;
