bc4818417a3322ddbf76f5b41d22446fd25f2b05
Hassan <hassanalgoz@gmail.com>
2017-10-12 15:46:58 +0300

    Comment out code related to firebase-admin.

--- utils.js
+++ utils.js
@@ -2,16 +2,16 @@ const path = require('path')
 const request = require('request-promise')
 const showdown  = require('showdown')
 const converter = new showdown.Converter()
-const firebaseAdmin = require("firebase-admin");
+// const firebaseAdmin = require("firebase-admin");
 // Configure
 // Markdown
 converter.setFlavor('github');
 // Firebase
-let serviceAccount = path.join(__dirname, '/credentials/kfum-clubs-firebase-adminsdk-384qk-b65ad5f692.json')
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(serviceAccount),
-  databaseURL: "https://kfum-clubs.firebaseio.com"
-});
+// let serviceAccount = path.join(__dirname, '/credentials/kfum-clubs-firebase-adminsdk-384qk-b65ad5f692.json')
+// firebaseAdmin.initializeApp({
+//   credential: firebaseAdmin.credential.cert(serviceAccount),
+//   databaseURL: "https://kfum-clubs.firebaseio.com"
+// });
 
 module.exports = {
     sendEmail: async function(body) {
@@ -33,18 +33,18 @@ module.exports = {
 
     // Note: You can send messages to up to 1,000 devices in a single request.
     // If you provide an array with over 1,000 registration tokens, the request will fail with a messaging/invalid-recipient error.
-    pushNotification: function(registrationTokens, notification) {
-        let payload = {
-            notification
-        }
-        firebaseAdmin.messaging().sendToDevice(registrationTokens, payload)
-        .then((response) => {
-          // See the MessagingDevicesResponse reference documentation for
-          // the contents of response.
-          console.log("Successfully sent message:", response);
-        })
-        .catch((error) => {
-          console.log("Error sending message:", error);
-        });
-    }
+    // pushNotification: function(registrationTokens, notification) {
+    //     let payload = {
+    //         notification
+    //     }
+    //     firebaseAdmin.messaging().sendToDevice(registrationTokens, payload)
+    //     .then((response) => {
+    //       // See the MessagingDevicesResponse reference documentation for
+    //       // the contents of response.
+    //       console.log("Successfully sent message:", response);
+    //     })
+    //     .catch((error) => {
+    //       console.log("Error sending message:", error);
+    //     });
+    // }
 };
